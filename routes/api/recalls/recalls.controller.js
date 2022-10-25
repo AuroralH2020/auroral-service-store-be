2015ee5b71f2bada87ab196a5f4f01e3ff265eec
AlbertoDiezBosonit <90448446+AlbertoDiezBosonit@users.noreply.github.com>
2022-10-25 16:27:18 +0200

    feat: change calls

--- routes/api/recalls/recalls.controller.js
+++ routes/api/recalls/recalls.controller.js
@@ -3,9 +3,11 @@
 var express = require('express');
 var router = express.Router();
 const httpService = require('@shared/services/http.service');
-const { host, port } = require('@config').actionsService;
+const { host, port, show_internal } = require('@config').actionsService;
 const languages = require('./languages');
 
+
+/*
 async function executePromises(promises, data, isIdService = false, isDataService = false) {
   let dataPromise = [];
   let ret = promises.length;
@@ -35,7 +37,7 @@ async function executePromises(promises, data, isIdService = false, isDataServic
           if (element.data.message.results != undefined)
             if (element.data.message.results.bindings != undefined)
               element.data.message.results.bindings.forEach((subElement) => {
-                if (subElement.g != undefined && subElement.g.value != undefined && subElement.g.value.length>7) {
+                if (subElement.g != undefined && subElement.g.value != undefined && subElement.g.value.length > 7) {
                   let data2 = {};
                   data2.agid = subElement.g.value.toString().substring(6);
                   data2.parameter = element.data.message.parameter;
@@ -81,72 +83,7 @@ async function executePromises(promises, data, isIdService = false, isDataServic
   return ret;
 }
 
-function parseLanguages(services) {
-  services.map(service => {
-    service.language = service.language.map(lan => {
-      const parsedLanguage = languages.getRArray().filter(language => language.Id == lan);
-      if (parsedLanguage != undefined && parsedLanguage.length > 0)
-        lan = parsedLanguage[0].Print_Name;
-      return lan;
-    }
-    );
-  });
-}
 
-function arraysToArrays(services) {
-  services.forEach(service => {
-    if (service.serviceName === undefined)
-      service.serviceName = [''];
-    if (typeof service.serviceName === 'string' || service.serviceName instanceof String)
-      service.serviceName = [service.serviceName];
-
-    if (service.versionOfService === undefined)
-      service.versionOfService = [''];
-    if (typeof service.versionOfService === 'string' || service.versionOfService instanceof String)
-      service.versionOfService = [service.versionOfService];      
-
-    if (service.serviceDescription === undefined)
-      service.serviceDescription = [''];
-    if (typeof service.serviceDescription === 'string' || service.serviceDescription instanceof String)
-      service.serviceDescription = [service.serviceDescription];
-
-    if (service.currentStatus === undefined)
-      service.currentStatus = [''];
-    if (typeof service.currentStatus === 'string' || service.currentStatus instanceof String)
-      service.currentStatus = [service.currentStatus];
-
-    if (service.hasDomain === undefined)
-      service.hasDomain = [''];
-    if (typeof service.hasDomain === 'string' || service.hasDomain instanceof String)
-      service.hasDomain = [service.hasDomain];
-
-    if (service.hasSubDomain === undefined)
-      service.hasSubDomain = [''];
-    if (typeof service.hasSubDomain === 'string' || service.hasSubDomain instanceof String)
-      service.hasSubDomain = [service.hasSubDomain];
-
-    if (service.hasFuncionality === undefined)
-      service.hasFuncionality = [''];
-    if (typeof service.hasFuncionality === 'string' || service.hasFuncionality instanceof String)
-      service.hasFuncionality = [service.hasFuncionality];
-
-    if (service.hasRequirement === undefined)
-      service.hasRequirement = [''];
-    if (typeof service.hasRequirement === 'string' || service.hasRequirement instanceof String)
-      service.hasRequirement = [service.hasRequirement];
-
-    if (service.serviceFree === undefined)
-      service.serviceFree = [false];
-    if (typeof service.serviceFree === 'boolean' || service.serviceFree instanceof Boolean)
-      service.serviceFree = [service.serviceFree];
-
-    if (service.language === undefined)
-      service.language = [''];
-    if (typeof service.language === 'string' || service.language instanceof String)
-      service.language = [service.language];
-
-  });
-}
 
 function insert_nodes_data(allData, nodesIds) {
   allData.forEach(data => {
@@ -250,7 +187,7 @@ async function insert_services_sparql(servicesAll, services, callsAtSameTime) {
   let promises = [], totalCalls = 0;
   let OIDs = await httpService.get(`http://${host}:${port}/api/registration`);
   let idMyNode = ''; // necesitamos una forma mejor de sacar la id del nodo
-  for (let i = 0; i < services.length; i++) {
+  for (let i = 0; i < services.length && idMyNode == ''; i++) {
     if (OIDs.data.message.includes(services[i].agid)) {
       idMyNode = services[i].parameter;
     }
@@ -262,8 +199,7 @@ async function insert_services_sparql(servicesAll, services, callsAtSameTime) {
       'SELECT * WHERE { GRAPH ?g { ?sub ?pred ?obj . ' +
       '} FILTER ( ?g IN (<graph:' + id + '> ))}'; // the body may change
     const config = { headers: { 'Content-Type': 'text/plain' } };
-    if (services[i].parameter != idMyNode || OIDs.data.message.includes(id)) {
-      // only services out of the node an in the node AND VALID
+    if (show_internal || services[i].parameter !== idMyNode) {
       id = services[i].parameter;
       promises.push(httpService.post(`http://${host}:${port}/api/discovery/remote/semantic/${id}`, body, config));
     }
@@ -274,6 +210,8 @@ async function insert_services_sparql(servicesAll, services, callsAtSameTime) {
   totalCalls += await executePromises(promises, servicesAll, false, true);
   return totalCalls;
 }
+*/
+
 /*
 async function insert_services(servicesAll, services, callsAtSameTime) {
   let promises = [], totalCalls = 0;
@@ -291,6 +229,166 @@ async function insert_services(servicesAll, services, callsAtSameTime) {
 }
 */
 
+function parseLanguages(services) {
+  services.map(service => {
+    service.language = service.language.map(lan => {
+      const parsedLanguage = languages.getRArray().filter(language => language.Id == lan);
+      if (parsedLanguage != undefined && parsedLanguage.length > 0)
+        lan = parsedLanguage[0].Print_Name;
+      return lan;
+    }
+    );
+  });
+}
+
+function arraysToArrays(services) {
+  services.forEach(service => {
+    if (service.serviceName === undefined)
+      service.serviceName = [''];
+    if (typeof service.serviceName === 'string' || service.serviceName instanceof String)
+      service.serviceName = [service.serviceName];
+
+    if (service.versionOfService === undefined)
+      service.versionOfService = [''];
+    if (typeof service.versionOfService === 'string' || service.versionOfService instanceof String)
+      service.versionOfService = [service.versionOfService];
+
+    if (service.serviceDescription === undefined)
+      service.serviceDescription = [''];
+    if (typeof service.serviceDescription === 'string' || service.serviceDescription instanceof String)
+      service.serviceDescription = [service.serviceDescription];
+
+    if (service.currentStatus === undefined)
+      service.currentStatus = [''];
+    if (typeof service.currentStatus === 'string' || service.currentStatus instanceof String)
+      service.currentStatus = [service.currentStatus];
+
+    if (service.hasDomain === undefined)
+      service.hasDomain = [''];
+    if (typeof service.hasDomain === 'string' || service.hasDomain instanceof String)
+      service.hasDomain = [service.hasDomain];
+
+    if (service.hasSubDomain === undefined)
+      service.hasSubDomain = [''];
+    if (typeof service.hasSubDomain === 'string' || service.hasSubDomain instanceof String)
+      service.hasSubDomain = [service.hasSubDomain];
+
+    if (service.hasFuncionality === undefined)
+      service.hasFuncionality = [''];
+    if (typeof service.hasFuncionality === 'string' || service.hasFuncionality instanceof String)
+      service.hasFuncionality = [service.hasFuncionality];
+
+    if (service.hasRequirement === undefined)
+      service.hasRequirement = [''];
+    if (typeof service.hasRequirement === 'string' || service.hasRequirement instanceof String)
+      service.hasRequirement = [service.hasRequirement];
+
+    if (service.serviceFree === undefined)
+      service.serviceFree = [false];
+    if (typeof service.serviceFree === 'boolean' || service.serviceFree instanceof Boolean)
+      service.serviceFree = [service.serviceFree];
+
+    if (service.language === undefined)
+      service.language = [''];
+    if (typeof service.language === 'string' || service.language instanceof String)
+      service.language = [service.language];
+
+  });
+}
+
+async function executePromisesDoAll(promises, data) {
+  let dataPromise = [];
+  const ret = promises.length;
+  await Promise.allSettled(promises).then(results => {
+    results.forEach((result) => {
+      let parameter = '';
+      if (result.status == 'fulfilled') {
+        let index = result.value.config.url.lastIndexOf('/');
+        parameter = result.value.config.url.substring(index + 1);
+        if (parameter != '')
+          result.value.data.message.parameter = parameter;
+        dataPromise.push(result.value);
+        console.log('Success');
+      }
+      else if (result.status == 'rejected') {
+        console.log('Error in promise: ', result.reason.toString(), ' path:', result.reason.request.path);
+      }
+    });
+  });
+  const namesKeysArrays = ['serviceName', 'serviceDescription', 'currentStatus', 'hasDomain',
+    'hasSubDomain', 'hasFuncionality', 'hasRequirement', 'serviceFree', 'language', 'versionOfService'];
+  const namesKeys = ['provider', 'dateLastUpdate', 'hasURL', 'applicableGeographicalArea',
+    'numberOfDownloads'];
+  let OIDs = await httpService.get(`http://${host}:${port}/api/registration`);
+  dataPromise.forEach((element) => {
+    if (element.data != undefined)
+      if (element.data.message != undefined)
+        if (element.data.message.results != undefined)
+          if (element.data.message.results.bindings != undefined) {
+            element.data.message.results.bindings.forEach((subElement) => {
+              if (subElement.p != undefined && subElement.p.value != undefined && subElement.p.value.includes('https://www.w3.org/2019/wot/td#')) {
+                let index = subElement.p.value.indexOf('#');
+                if (index >= 0 && subElement.o != undefined && subElement.o.value) {
+                  let agid = '';
+                  agid = subElement.sub.value.substring('https://oeg.fi.upm.es/wothive/'.length);
+                  if (show_internal || !OIDs.data.message.includes(agid)) {
+                    let data2 = data.find((a) => a.agid == agid);
+                    if (data2 == undefined) {
+                      data2 = { agid: agid };
+                      data.push(data2);
+                    }
+                    let key = subElement.p.value.substring(index + 1);
+                    if (namesKeysArrays.includes(key)) {
+                      if (data2[key] == undefined)
+                        data2[key] = [subElement.o.value];
+                      else
+                        data2[key].push(subElement.o.value);
+                    }
+                    else if (namesKeys.includes(key)) {
+                      data2[key] = subElement.o.value;
+                    }
+                  }
+                }
+              }
+            });
+          }
+  });
+  promises.splice(0, promises.length);
+  return ret;
+}
+
+async function servicesDoAll(allData, servicesAll, callsAtSameTime) {
+  // do all the work
+  let communitiesIds = [], promises = [], totalCalls = 0;
+  communitiesIds = await httpService.get(`http://${host}:${port}/api/collaboration/communities`);
+  communitiesIds.data.message.forEach(community => {
+    allData.push(community);
+  });
+  console.log('Comunities: ', allData);
+  communitiesIds = communitiesIds.data.message;
+  for (let i = 0; i < communitiesIds.length; i++) {
+    let id = communitiesIds[i].commId;
+    const body = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' +
+      'PREFIX wot: <https://www.w3.org/2019/wot/td#> ' +
+      'SELECT distinct ?p ?o ?sub WHERE {  ' +
+      '?sub rdf:type wot:Service . ' +
+      '?sub ?p ?o . } '; // the body may change
+    const config = { headers: { 'Content-Type': 'text/plain' } };
+    promises.push(httpService.post(`http://${host}:${port}/api/discovery/remote/semantic/community/${id}`, body, config));
+    if (promises.length >= callsAtSameTime) {
+      totalCalls += await executePromisesDoAll(promises, servicesAll);
+    }
+  }
+  totalCalls += await executePromisesDoAll(promises, servicesAll);
+  //insert_nodes_data(allData, servicesAll);
+  return totalCalls;
+
+
+}
+
+
+
+
 class CallsService {
 
   constructor() {
@@ -298,37 +396,47 @@ class CallsService {
     this.url = `http://${host}:${port}`;
     this.router = router;
     this.callsAtSameTime = 100;
-    this.router.use(function timeLog(req, res, next) {
+    /*this.router.use(function timeLog(req, res, next) {
       console.log('Time: ', Date.now());
       next();
-    });
+    });*/
 
     this.router.get('/services', async function (req, res) {
       try {
-        let services = [], nodesIds = [];
-        let allData = [], servicesAll = [];;
+        let allData = [], servicesAll = [];
         const callsAtSameTime = 100;
         let totalCalls = 1;
-        totalCalls += await insert_communities_nodes(allData, nodesIds, callsAtSameTime);
+        const beginTime = Date.now();
+        let services = [], nodesIds = [];                
+        /*totalCalls += await insert_communities_nodes(allData, nodesIds, callsAtSameTime);
         totalCalls += await insert_services_ids(nodesIds, allData, services, callsAtSameTime);
         totalCalls += await insert_services_sparql(servicesAll, services, callsAtSameTime);
         arraysToArrays(servicesAll); // parse data
         parseLanguages(servicesAll); // parse data language
         insert_all_in_services_data(allData, servicesAll); // parse data
         for (let i = 0; i < 1 && i < servicesAll.length; i++) {
-          console.log('servicio',servicesAll[i]);
+          console.log('servicio', servicesAll[i]);
         }
         allData.forEach(community => {
           console.log(community);
-          let counter =0;
+          let counter = 0;
           community.nodes.forEach(node => {
-            if (counter<10){
+            if (counter < 10) {
               console.log('Node: ', node);
               counter++;
             }
           });
         });
         console.log('Llamadas realizadas en total: ', totalCalls);
+        */
+        servicesAll = [];
+        allData = [];
+        totalCalls += await servicesDoAll(allData, servicesAll, callsAtSameTime);
+        arraysToArrays(servicesAll); // parse data
+        parseLanguages(servicesAll); // parse data language
+        console.log('Number of calls: ', totalCalls);
+        console.log('Services send: ', servicesAll);
+        console.log('Time: ', (Date.now() - beginTime)/1000, ' seconds' );
         res.send({ result: servicesAll });
       }
       catch (e) {
